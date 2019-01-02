7b2b667b1d97d926f211ce98c9df34b94aade7c6
unknown <mahmudul2843@gmail.com>
2019-01-03 00:30:24 +0600

    first commit

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php include('headerfooter.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="home.css">
+</head>
+<body>
+<?php include('slideshow.php');?>
+<div class="contact">
+	<p>CONTACT:<br>Mahmudul Hasan<br>East West University<br>Cell:01823472843 Email:mahmudul2843@gmail.com</p>
+</div>
+
+</body>
+</html>
